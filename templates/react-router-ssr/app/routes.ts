34e6f8e67cd4d6a7111e0f7d2822d128940d5ea3
Ryan Bower <rbower@qti.qualcomm.com>
2026-01-01 18:08:17 -0800

    fix: add vite.config.ts to react-router-ssr template

--- templates/react-router-ssr/app/routes.ts
+++ templates/react-router-ssr/app/routes.ts
@@ -2,8 +2,5 @@ import {index, layout, route, type RouteConfig} from "@react-router/dev/routes"
 
 export default [
   route("action/set-theme", "./routes/action.set-theme.ts"),
-  layout("./routes/layout.tsx", [
-    index("./routes/home.tsx"),
-    route("register", "./routes/register.tsx"),
-  ]),
+  layout("./routes/layout.tsx", [index("./routes/home.tsx")]),
 ] satisfies RouteConfig
